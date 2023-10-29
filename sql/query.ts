f277cfa60fb395d6077cff66d478f34dacfca2e8
Rafael Costa <rafael@cos.tax>
2023-10-29 16:57:14 +0100

    Updates .gitignore, query.ts

--- sql/query.ts
+++ sql/query.ts
@@ -6,34 +6,37 @@ import moment from "moment";
 // const query = db.query("select 'Hello world' as message;");
 // query.get(); // => { message: "Hello world" }
 
+class DatabaseHolder {
+    database!: Database
+
+    constructor(path: string) {
+        this.database = new Database(path);    
+    }
+}
+
 class Query {
-    connection_map!: Map<string, Database>;
+    databaseHolder!: DatabaseHolder;
 
     constructor() {
-        this.connection_map = new Map();
+        this.databaseHolder = new DatabaseHolder('./db.sqlite');
     }
 
-    _containsForDate(date: string): boolean {
-        return this.connection_map.has(date);
-    }
-
-    async queryWithParameters(start_date: string, end_date: string, query: string): Promise<Entry> {
+    async queryWithParameters(start_date: string, end_date: string, conditions: string): Promise<Entry> {
         let start_date_parsed = moment(start_date);
         let end_date_parsed = moment(end_date);
         let days_between = end_date_parsed.clone().subtract(start_date).days();
+
+        let statements = [];
         
         for (let day = 0; day < days_between; day++) {
             const date = start_date_parsed.clone().add(day, 'days');
             const date_string = date.format('YYYYMMDD');
-
-            if (this._containsForDate(date_string)) {
-                // We already have a connection for that date.
-                // TODO: (objectivecosta) Parse on existing connection for that date.
-            } else {
-                // We DON'T have a connection for that date.
-                // TODO: (objectivecosta) Create connection for that date.
-            }
+            let tableName = `traffic_${date_string}`;
+            let statement = this.databaseHolder.database.query(`SELECT * FROM ${tableName} WHERE ${conditions}`);
+            statements.push(statement)
         }
+
+        let results = statements.map((s) => s.all());
 
         return new Entry();
     }
